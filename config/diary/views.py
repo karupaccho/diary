6892f67cf9e10adfb9c2951e74bd595fa3574ce1
karupaccho <k.utsugi021@gmail.com>
2021-09-14 20:45:50 +0900

    9/14夜

--- config/diary/views.py
+++ config/diary/views.py
@@ -1,3 +1,4 @@
+from config import diary
 from .forms import InquiryForm
 from django.http.response import HttpResponse
 from django.shortcuts import render
@@ -38,3 +39,30 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         diaries = Diary.objects.filter(user = self.request.user).order_by('-created_at')
         return diaries
+
+class DetailView(generic.DeleteView):
+    model = Post
+    slug_field = "title"
+    slug_url_kwarg = "title"
+
+class DiaryDetailView(LoginRequiredMixin,generic.DeleteView):
+    model = Diary
+    template_name = 'diary/diary_detail.html'
+    pk_url_kwarg = 'id'
+
+class DiaryCreateView(LoginRequiredMixin,generic.CreateView):
+    model = Diary
+    template_name = "diary_create.html"
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit = False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request,'日記を作成しました。')
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request,'日記の作成に失敗しました')
+        return super().form_invalid(form)
