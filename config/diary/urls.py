dd2be09c160473df61ae619aea54f8db3e90ca42
utsugi <k.utsugi021@gmail.com>
2021-09-14 12:31:30 +0900

    9/14コミット

--- config/diary/urls.py
+++ config/diary/urls.py
@@ -1,3 +1,4 @@
+from diary.models import Diary
 from diary.forms import InquiryForm
 from django.urls import path
 from .import views
@@ -5,5 +6,6 @@ from .import views
 app_name = 'diary'
 urlpatterns = [
     path('',views.IndexView.as_view(),name='index'),
-    path('inquiry/', views.InquiryView.as_view(), name = "inquiry")
+    path('inquiry/', views.InquiryView.as_view(), name = "inquiry"),
+    path('diary_list/',views.DiaryListView.as_view(),name = 'diary_list'),
 ]
