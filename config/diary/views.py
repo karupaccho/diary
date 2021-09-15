71c951b5b456d0f922c47f72742b99723369ece5
karupaccho <k.utsugi021@gmail.com>
2021-09-15 20:40:23 +0900

    9/15 夜

--- config/diary/views.py
+++ config/diary/views.py
@@ -1,5 +1,6 @@
-from config import diary
-from .forms import InquiryForm
+# from config import diary
+from django.contrib.messages.api import success
+from .forms import InquiryForm, DiaryCreateForm
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views import generic
@@ -40,12 +41,12 @@ class DiaryListView(LoginRequiredMixin, generic.ListView):
         diaries = Diary.objects.filter(user = self.request.user).order_by('-created_at')
         return diaries
 
-class DetailView(generic.DeleteView):
-    model = Post
-    slug_field = "title"
-    slug_url_kwarg = "title"
+# class DetailView(generic.DeleteView):
+#     model = Post
+#     slug_field = "title"
+#     slug_url_kwarg = "title"
 
-class DiaryDetailView(LoginRequiredMixin,generic.DeleteView):
+class DiaryDetailView(LoginRequiredMixin,generic.DetailView):
     model = Diary
     template_name = 'diary/diary_detail.html'
     pk_url_kwarg = 'id'
@@ -66,3 +67,28 @@ class DiaryCreateView(LoginRequiredMixin,generic.CreateView):
     def form_invalid(self, form):
         messages.error(self.request,'日記の作成に失敗しました')
         return super().form_invalid(form)
+
+class DiaryUpdateView(LoginRequiredMixin,generic.UpdateView):
+    model = Diary
+    template_name = 'diary_update.html'
+    form_class = DiaryCreateForm
+
+    def get_success_url(self):
+        return reverse_lazy('diary:diary_detail',kwargs = {'pk':self.kwargs['pk']})
+    
+    def form_valid(self, form):
+        messages.success(self.request,'日記を更新しました')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request,"日記の更新に失敗しました")
+        return super().form_invalid(form)
+
+class DiaryDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Diary
+    template_name = 'diary_delete.html'
+    success_url = reverse_lazy('diary:siary_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request,"日記を削除しました")
+        return super().delete(request, *args, **kwargs)
