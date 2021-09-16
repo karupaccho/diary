e85d0e4fdd1b74758799e902f2c382a05631aea2
utsugi <k.utsugi021@gmail.com>
2021-09-16 13:06:48 +0900

    9/16コミット

--- config/config/urls.py
+++ config/config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.staticfiles.urls import static
-from django.urls.conf import path, include
+from django.urls import path, include
 
 from . import settings
 
